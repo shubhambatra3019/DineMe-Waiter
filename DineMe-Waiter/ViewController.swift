1f1776785329af6b4169e65bab743f8a950d22f5
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2019-02-01 13:41:10 -0500

    Initial Commit

---
+++ DineMe-Waiter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DineMe-Waiter
+//
+//  Created by Shubham Batra on 01/02/19.
+//  Copyright © 2019 Esper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
